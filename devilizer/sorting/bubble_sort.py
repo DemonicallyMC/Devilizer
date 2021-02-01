0269ac4aa1f5936279b5a793e70a607789dada51
SuperMaZingCoder <supermazingcoder@gmail.com>
2021-02-01 10:57:18 -0500

    Improved explicitness in bubble sort loop.

--- devilizer/sorting/bubble_sort.py
+++ devilizer/sorting/bubble_sort.py
@@ -8,12 +8,12 @@ def bubble_sort(in_lst: list) -> list:
         list: Sorted list.
     """
     out_lst = in_lst.copy()
-    changes = 1
-    while changes:
-        changes = 0
+    done = False
+    while not done:
+        done = True
         for i in range(len(out_lst)-1):
             if out_lst[i] > out_lst[i+1]:
                 out_lst[i], out_lst[i+1] = out_lst[i+1], out_lst[i] 
-                changes += 1
+                done = True
             yield out_lst, i+1
     return out_lst
